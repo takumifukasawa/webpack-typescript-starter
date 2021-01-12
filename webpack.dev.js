411dc16d9f09303a5abe8e4a7abdba21f5762da2
takumifukasawa <fukapon912@gmail.com>
2021-01-12 22:50:50 +0900

    format and uninstall rimraf

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,22 +1,22 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const path = require('path');
-const readConfig = require('read-config');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const path = require("path");
+const readConfig = require("read-config");
 
-const constants = readConfig('./constants.yml');
+const constants = readConfig("./constants.yml");
 
 const { DEST, BASE_DIR } = constants;
 
 // base config
-const HOST = process.env.HOST || '0.0.0.0';
+const HOST = process.env.HOST || "0.0.0.0";
 const PORT = process.env.PORT || 3000;
 
 module.exports = merge(common, {
-    mode: 'development',
-    devServer: {
-      host: HOST,
-      port: PORT,
-      contentBase: DEST,
-      openPage: path.relative('/', BASE_DIR),
-    },
+  mode: "development",
+  devServer: {
+    host: HOST,
+    port: PORT,
+    contentBase: DEST,
+    openPage: path.relative("/", BASE_DIR),
+  },
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,6 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
-// TODO: fix for assets
-// common.plugins.push(new CleanWebpackPlugin());
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
 });
