dd876b1da394148c868c9015a991230673255b21
takumifukasawa <fukapon912@gmail.com>
2021-01-12 21:22:04 +0900

    prettier

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,9 +1,9 @@
-import Sample from '~/lib/Sample';
+import Sample from "~/lib/Sample";
 
-const name: string = 'world';
+const name: string = "world";
 
 const sample = new Sample(name);
 
-document.querySelector('.wrapper').addEventListener('click', () => {
+document.querySelector(".wrapper").addEventListener("click", () => {
   console.log(`hello, ${sample.name}.`);
 });
